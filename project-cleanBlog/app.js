9abe520a07a6d78cc1718c99e1f83d69bf6d7f28
Tolgahan Tolu <tolgahanntolu@gmail.com>
2023-07-01 11:01:10 +0300

    template engine: ejs

--- project-cleanBlog/app.js
+++ project-cleanBlog/app.js
@@ -1,13 +1,31 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
 
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+// MIDDLEWARE
+app.use(express.static('public'));
+
+// ROUTES
 app.get('/', (req, res) => {
-  const blog = {
-    id: 1,
-    title: 'Blog title',
-    description: 'Blog description',
-  };
-  res.send(blog);
+  //  const blog = {
+  //    id: 1,
+  //    title: 'Blog title',
+  //    description: 'Blog description',
+  //  };
+  //  res.send(blog);
+  res.render('index');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
 });
 
 const port = 3000 || process.env.PORT;
